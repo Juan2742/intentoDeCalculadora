61e247c63ae2eec89328caf2111204cf859999f7
Juan2742 <fonsecajuan239@gmail.com>
2023-02-25 21:55:28 -0500

    Primera fase de la calculadora

---
+++ calculadorajs/calculadora.js
@@ -0,0 +1,14 @@
+    let primerNumero 
+    let segundoNumero 
+
+function inicio (){
+
+    let primerDigito = document.getElementById ("primerDigito")
+    let segundoDigito = document.getElementById ("segundoDigito") 
+
+    let primerNumero = primerDigito
+    let segundoNumero = segundoDigito
+
+alert(primerNumero + " " + segundoNumero)
+
+}
