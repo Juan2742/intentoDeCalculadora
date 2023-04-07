f09c72b2152e83cf44c5085b2856e6c168e5c188
Juan2742 <fonsecajuan239@gmail.com>
2023-04-06 20:18:04 -0500

    Ya esta implementada la primera fase de las operaciones, solo falta la mitad y resolver un problema relacionado con la suma de los digitos

--- calculadorajs/calculadora.js
+++ calculadorajs/calculadora.js
@@ -15,38 +15,75 @@ function inicio (){
     let inputMultiplicacion = document.getElementById("multiplicacion")
     let inputDivision = document.getElementById("division")
 
-if (inputSuma){
-    operacionSuma ()
+if (inputSuma.checked){
+    alert("Tu sumaras " + primerNumero + " mas " + segundoNumero)
+    alert("El resultado aparecera debajo del boton Realizar")
+    let resultado = primerNumero + segundoNumero
+    operacionSuma (primerNumero, segundoNumero, resultado)
 
 }
-else if (inputResta){
+else if (inputResta.checked){
+    alert("Tu restaras " + primerNumero + " menos " + segundoNumero)
+    alert("El resultado aparecera debajo del boton Realizar")
+    let resultado = primerNumero - segundoNumero
+    operacionResta(primerNumero, segundoNumero, resultado)
 
 
 }
-else if (){
-
+else if (inputMultiplicacion.checked){
+    alert("Tu multiplicaras " + primerNumero + " por " + segundoNumero)
+    alert("El resultado aparecera debajo del boton Realizar")
+    operacionMultiplicacion(primerNumero, segundoNumero, resultado)
     
 }
-else if (){
-
+else if (inputDivision.checked){
+    alert("Tu dividiras " + primerNumero + " dividido entre " + segundoNumero)
+    alert("El resultado aparecera debajo del boton Realizar")
+    operacionDivision(primerNumero, segundoNumero, resultado)
     
 }
 else {
-
-
-}
-
-    
+    alert("Necesitas una operacion. Se reiniciara la pagina.")
+    reiniciar()
 
 }
 
-function operacionSuma (){
+}
 
+function operacionSuma (primerNumero, segundoNumero, resultado){
+    let sectionMensajes = document.getElementById("mensajes")
+    let parrafo = document.createElement("p")
 
+    parrafo.innerHTML = primerNumero + " + " + segundoNumero + " = " + resultado 
+
+    sectionMensajes.appendChild(parrafo)
+
+}
+function operacionResta(primerNumero, segundoNumero, resultado){
+    let sectionMensajes = document.getElementById("mensajes")
+    let parrafo = document.createElement("p")
+
+    parrafo.innerHTML = primerNumero + " - " + segundoNumero + " = " + resultado
+
+    sectionMensajes.appendChild(parrafo)
+
+}
+function operacionMultiplicacion(primerNumero, segundoNumero, resultado){
 
 
 }
-function crearMensaje(operacion){
+function operacionDivision(primerNumero, segundoNumero, resultado){
+
+
+}
+function reiniciar(){
+    location.reload()
+
+}
+function crearMensaje(resultado){
+    let sectionMensajes = document.getElementById("mensajes")
+    let parrafo = document.createElement("p")
+
 
 
 }
